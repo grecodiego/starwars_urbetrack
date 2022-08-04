5e36a94766ff1d5dcdfeb1b7ce28f442072dde4c
Diego Greco <greco.diego@gmail.com>
2022-08-04 15:19:46 -0300

    create new file with charDetails model

---
+++ lib/screens/CharDetails/models/char_details_model.dart
@@ -0,0 +1,8 @@
+import 'package:starwars_urbetrack/screens/CharDetails/models/starship_model.dart';
+import 'package:starwars_urbetrack/screens/CharDetails/models/vehicles_model.dart';
+
+class CharDetails {
+  String charWorldName = '';
+  List<StarshipModel> charStarships = [];
+  List<VehiclesModel> charVehicles = [];
+}
